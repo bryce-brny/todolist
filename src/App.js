52686c041467e16ff726bb5b86ce9976b8769754
bryce-brny <brightbrgh@gmail.com>
2023-04-24 16:14:20 +0700

    finish:Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // import logo from './logo.svg';
 import './App.scss';
+import { FaHome } from 'react-icons/fa'
 
 function App() {
   return (
   <div className='container'>
-    <header className='header'>Header</header>
+    <header className='header'>
+      <span>
+        <FaHome/>
+      </span>
+      
+      <h3>CC-Todolist</h3>
+      
+    </header>
 
     <aside className='sidebar'>Sidebar</aside>
 
