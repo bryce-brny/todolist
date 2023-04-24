b3c2fc41bd197605998c4b6d61e9e804aa9f3fc3
bryce-brny <brightbrgh@gmail.com>
2023-04-24 16:58:31 +0700

    add project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.scss';
-import { FaHome } from 'react-icons/fa'
+import { FaHome ,FaInbox,FaCalendar,FaCalendarAlt,FaChevronDown } from 'react-icons/fa'
+
 
 function App() {
   return (
@@ -14,7 +15,53 @@ function App() {
       
     </header>
 
-    <aside className='sidebar'>Sidebar</aside>
+    <aside className='sidebar'>
+     <section className='sidebar__generic'>
+     <ul className='generic__lists'>
+        <li className='action'>
+          <span>
+              <FaInbox />
+          </span>
+          <h6>Inbox</h6> 
+        </li>
+        <li>
+          <span>
+              <FaCalendar />
+          </span>
+          <h6>Today</h6> 
+        </li>
+        <li>
+          <span>
+              <FaCalendarAlt />
+          </span>
+          <h6>Next 7 Days</h6> 
+        </li>
+     </ul>
+     </section> 
+     <section className='sidebar__middle'>Projects</section> 
+     
+     <section className='sidebar__projects'>
+    <ul className='project__lists'>
+    <li className=''>
+          <span>
+              <FaChevronDown />
+          </span>
+          <h6>ProjectA</h6> 
+        </li>
+        <li>
+          <span>
+              <FaChevronDown />
+          </span>
+          <h6>ProjectA</h6> 
+        </li>
+    </ul>
+
+
+
+     </section> 
+    
+      
+    </aside>
 
     <main>Content</main>
     
