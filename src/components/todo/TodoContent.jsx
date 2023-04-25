68a09ef93950d7064bbd3cdd858df59c60b56c36
bryce-brny <brightbrgh@gmail.com>
2023-04-25 15:56:23 +0700

    finish todos

--- src/components/todo/TodoContent.jsx
+++ src/components/todo/TodoContent.jsx
@@ -1,13 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+import {useState} from 'react'
 import { AddTodo } from './AddTodo';
 import { TodoHeader } from './TodoHeader';
 import { TodoLists } from './TodoLists';
+import mockData from '../../todos.json';
 
 export function TodoContent() {
-    return (
-        <main className='content'>
-            <TodoHeader />
-            <AddTodo />
-            <TodoLists />
-        </main>
-    );
+  // # Logic
+  const [todos, setTodos] = useState(mockData);
+
+  const handleAddTodo = () => {
+      // มี new todo
+      let newTodoObj = { id: uuidv4(), task: 'DoHW', status: false, due_date: '' };
+
+      // สร้าง state ใหม่
+      // update state โดย new state
+      // const newTodos = [newTodoObj, ...todos];
+      // setTodos(newTodos);
+
+      // update state โดย callback
+      setTodos(currentState=> [newTodoObj,...currentState])
+  };
+
+  // # UI
+  return (
+      <main className='content'>
+          <TodoHeader />
+          <AddTodo />
+          <button onClick={handleAddTodo}>Test Add Todo</button>
+          <TodoLists todos={todos} />
+      </main>
+  );
 }
