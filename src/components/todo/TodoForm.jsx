a5d7b049cb37a4afd072d4f2ed7170dfc86617b8
bryce-brny <brightbrgh@gmail.com>
2023-04-25 12:12:07 +0700

    finish

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -1,17 +1,50 @@
-import styles from './TodoForm.module.scss'
+import styles from './TodoForm.module.scss';
+import React, { useState } from 'react';
 
-export function TodoForm () {
+export function TodoForm({onSetIsAddMode}) {
+    // # 1 : Logic Section
+    const [task, setTask] = useState('');
 
-	return (
-		<form className={styles.todo__form__container}>
-		<input className={styles.todo__form__input} placeholder='Task Name' />
-		<div className={styles.todo__form__footer}>
-			<p className={styles.todo__error}>Title is required</p>
-			<div className={styles.todo__form__buttons}>
-				<button>Cancel</button>
-				<button>Add Task</button>
-			</div>
-		</div>
-	</form>
-	)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submit');
+		// จบ AddMode
+		// setIsAddMode(false)
+		// a === setIdAddMode
+		onSetIsAddMode(false)
+		// setTask('')
+    };
+    const handleClickCancel = (e) => {
+        console.log('cancel');
+		onSetIsAddMode(false)
+		// setTask('')
+
+    };
+
+    const handleChangeInput = (e) => {
+        // console.log(e.target.value);
+		setTask(e.target.value)
+    };
+
+    // # 2 : UI-Section
+    return (
+        <form className={styles.todo__form__container} onSubmit={handleSubmit}>
+            <input
+                className={styles.todo__form__input}
+                placeholder='Task Name'
+                value={task}
+                onChange={handleChangeInput}
+            />
+            <div className={styles.todo__form__footer}>
+                <p className={styles.todo__error}>Title is required</p>
+                <div className={styles.todo__form__buttons}>
+                    <button type='button' onClick={handleClickCancel}>
+                        Cancel
+                    </button>
+                    <button type='submit'>Add Task</button>
+                </div>
+            </div>
+        </form>
+    );
 }
+
