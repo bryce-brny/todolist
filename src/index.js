9f99d813ee3c0d366dfa51f6a94d3a81d074ae95
bryce-brny <brightbrgh@gmail.com>
2023-04-26 16:08:24 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss'; // Global CSS
+import App from './app/App';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
